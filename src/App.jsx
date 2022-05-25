6b9371c1c618f623ebc6669bfaf3f40a7745d9cb
Seth Bunke <sjb_strat@hotmail.com>
2022-05-25 15:42:17 -0700

    fix messge

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './index.css'
 
 const App = () => {
-  const x = 'test info1b';
+  const x = 'test info';
   //console.log("this is my tests");
   return (
     <h1 className="underline text-3xl">App - { x } test</h1>
