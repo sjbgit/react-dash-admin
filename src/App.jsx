4b18e6b1a309ee499b631d771f695147b3331ff1
Seth Bunke <sjb_strat@hotmail.com>
2022-05-25 15:13:54 -0700

    update lauch config for debug

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
 const App = () => {
+  const x = 'test info1';
+  console.log("this is my tests");
   return (
-    <div>App - test</div>
+    <div>App - { x } test</div>
   )
 }
 
